8d49859f02d3b5a6755854497f393ae49b30fa02
Christopher Carrasco <chris.carrasco@att.net>
2023-02-21 13:54:47 -0600

    first commit, video time stamp 19:16

---
+++ src/main/java/org/guymanpersonboy/Main.java
@@ -0,0 +1,59 @@
+package org.guymanpersonboy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.guymanpersonboy.Main.Gender.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", NONBINARY),
+                new Person("Alice", FEMALE)
+        );
+
+        System.out.println("Imperative approach");
+        // Imperative approach
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
+
+        System.out.println("Declarative approach");
+        // Declarative approach
+        List<Person> females2 = people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + gender.toString();
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE, NONBINARY
+    }
+}
