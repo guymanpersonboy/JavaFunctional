237e9480315cbedbe254fe346c819a38ccbc8908
Christopher Carrasco <chris.carrasco@att.net>
2023-02-22 14:41:49 -0600

    Supplier, basic Stream, video time stamp 1:19:26

---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,55 @@
+package streams;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.*;
+
+public class _Stream {
+
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", NONBINARY),
+                new Person("Alice", FEMALE)
+        );
+
+        people.stream()
+                .map(person -> person.gender)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        Function<Person, String> personStringFunction = person -> person.name;
+        ToIntFunction<String> length = String::length;
+        IntConsumer println = System.out::println;
+
+        people.stream()
+                .map(personStringFunction)
+                .mapToInt(length)
+                .forEach(println);
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + gender.toString();
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE, NONBINARY
+    }
+}
