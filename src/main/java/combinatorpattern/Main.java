183ca896650bf8672ee91d3f7dec3c7a1eaa92a0
Christopher Carrasco <chris.carrasco@att.net>
2023-02-24 16:01:31 -0600

    Optional, combinator pattern, video time stamp 1:55:36

---
+++ src/main/java/combinatorpattern/Main.java
@@ -0,0 +1,33 @@
+package combinatorpattern;
+
+import java.time.LocalDate;
+
+import static combinatorpattern.CustomerRegistrationValidator.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer(
+                "Alice",
+                "alice@gmail.com",
+                "+12345678",
+                LocalDate.of(2000, 1, 1)
+        );
+
+        // Does not show which customer property is not valid
+//        System.out.println(new CustomerValidatorService().isValid(customer));
+
+        // Using combinator pattern
+        ValidationResult result = isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
+    }
+
+}
